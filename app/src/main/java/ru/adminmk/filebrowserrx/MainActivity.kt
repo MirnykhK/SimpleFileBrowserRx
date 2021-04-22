bceba0c15c2c46d51f8afbe339b16f9712ab7cbc
MirnykhK <adminmk@mail.ru>
2021-04-22 17:46:17 +0500

    start

---
+++ app/src/main/java/ru/adminmk/filebrowserrx/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.adminmk.filebrowserrx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
